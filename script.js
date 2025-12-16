49ecde4d10dd8148cb69febc364f5785282b30e9
abdoAA96 <abdalrahmanalaa7@gmail.com>
2025-12-16 04:35:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript Function (Requirement 7)
+function showAlert() {
+    // This simple function demonstrates the use of a JavaScript function.
+    alert("Function Executed! This proves the JavaScript file is correctly linked and running.");
+}
